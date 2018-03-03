6613ed67196533ab314c5a663975fdccef5c50a5
Laurent PELLET <laurentP@ispf.local>
2018-03-03 10:24:45 -1000

    Ajouts des acteurs

--- Main.R
+++ Main.R
@@ -1,5 +1,10 @@
 library(jsonlite)
+
 questions<-fromJSON("Questions_gouvernement_XIV.json")
+acteurs<-fromJSON("AMO10_deputes_actifs_mandats_actifs_organes_XIV.json")
+
+acteursDF<-acteurs$export$acteurs$acteur$etatCivil
+
 index<-questions$questionsGvt$question$indexationAN
 auteur<-questions$questionsGvt$question$auteur
 textes<-questions$questionsGvt$question$textesReponse
