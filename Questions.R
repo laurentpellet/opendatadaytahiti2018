f44cba82e25971dc11791296328e3703805b4edc
Laurent PELLET <laurentP@ispf.local>
2018-03-03 11:38:24 -1000

    Questions mises en forme

--- Questions.R
+++ Questions.R
@@ -1,20 +1,21 @@
 library(jsonlite)
 library(data.table)
+library(XML)
+library(RCurl)
 
-questions<-fromJSON("Questions_gouvernement_XIV.json")
-q<-questions$questionsGvt$question
+q<-fromJSON("Questions_gouvernement_XIV.json")
+q<-q$questionsGvt$question
+pattern <- "</?\\w+((\\s+\\w+(\\s*=\\s*(?:\".*?\"|'.*?'|[^'\">\\s]+))?)+\\s*|\\s*)/?>"
 
 q1<-cbind(
   q$uid,
   q$identifiant,
   q$indexationAN$rubrique,
-  q$indexationAN$teteAnalyse)
-  
+  q$indexationAN$teteAnalyse,
+  unlist(lapply(q$indexationAN$analyses$analyse, FUN=function(x)paste0(x,collapse = ","))),
+  q$auteur$identite,
+  q$auteur$groupe,
+  q$minInt,
+  gsub(pattern, "\\1", q$textesReponse$texteReponse$texte))
 
-
-
-auteur<-questions$questionsGvt$question$auteur
-textes<-questions$questionsGvt$question$textesReponse
-
-
-table(index$rubrique)
+colnames(q1)[c(1,5,6,7,13,14,15)]<-c("uid", "rubrique", "teteAnalyse", "motsCles","minIntAbrege","minIndDeveloppe","question")
