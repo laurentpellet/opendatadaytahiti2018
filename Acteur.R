86239dda866c0319d561a92e499e02d2e83da191
HLeviel <HLeviel@users.noreply.github.com>
2018-03-03 11:54:44 -1000

    Add files via upload

--- Acteur.R
+++ Acteur.R
@@ -1,8 +1,9 @@
 acteur <- acteurs$export$acteurs$acteur
-acteursDF<-cbind(acteur$etatCivil$ident,
-                 acteur$profession$libelleCourant,
-                 acteur$mandats$mandat)
-names(acteur$mandats$mandat[[1]])
-names(acteurs$export$organes$organe)
-head(acteurs$export$organes$organe$positionPolitique)
+acteursDF<-cbind(ActeurRef=acteur$uid$`#text`, 
+                 acteur$etatCivil$ident,
+                 acteur$profession$socProcINSEE,
+                 acteur$profession$libelleCourant)
+
+
+table(acteur$profession$socProcINSEE)
 head(acteurs$export$acteurs$acteur$profession)
